af26d31a123aac53236230fb872619b72df9a40c
ZCode GitHub <zcode.github@gmail.com>
2026-02-28 17:19:59 -0300

    add downloads script and terraform binary file

---
+++ tools/downloads.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Bash safety flags:
+# -e: exit on any error
+# -u: treat unset variables as an error and exit
+# -o pipefail: fail if any command in a pipeline fails
+set -euo pipefail
+
+# Set the Github repository directory
+GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+# Set the Github runner os-type
+RUNNER_OSTYPE=linux-gnu
+
+# ---------------------------------------------
+# List for AMD64 machines
+# ---------------------------------------------
+# TERRAFORM from https://releases.hashicorp.com/terraform
+TERRAFORM_VERSION=1.10.5
+
+# for Linux local machine
+# curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
+
+# for Windows local machine
+curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_windows_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
+
+# for MacOS local machine
+# curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
+
+# For Github runner pipeline
+curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${RUNNER_OSTYPE}.zip"
