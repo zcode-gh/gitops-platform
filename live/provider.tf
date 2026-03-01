a6038eed5450198b935eeb5037cd14675ad5f472
ZCode GitHub <zcode.github@gmail.com>
2026-03-01 09:46:47 -0300

    add vpc resource

---
+++ live/provider.tf
@@ -0,0 +1,15 @@
+# Specify the provider
+provider "aws" {
+  region = "us-east-1"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  access_key = "foo"
+  secret_key = "bar"
+
+  endpoints {
+    ec2 = "http://localhost:4566" # localstack settings to emulate the AWS VPC deployment
+  }
+}
