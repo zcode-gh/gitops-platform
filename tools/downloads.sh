30f91014ebcac89bfa0e51a72c61102cda27e617
ZCode GitHub <zcode.github@gmail.com>
2026-03-01 14:09:04 -0300

    add tflint binaries and settings

--- tools/downloads.sh
+++ tools/downloads.sh
@@ -16,16 +16,33 @@ RUNNER_OSTYPE=linux-gnu
 # List for AMD64 machines
 # ---------------------------------------------
 # TERRAFORM from https://releases.hashicorp.com/terraform
-TERRAFORM_VERSION=1.10.5
+# TERRAFORM_VERSION=1.10.5
 
 # for Linux local machine
 # curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
 
 # for Windows local machine
-curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_windows_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
+# curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_windows_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
 
 # for MacOS local machine
 # curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${OSTYPE}.zip"
 
 # For Github runner pipeline
-curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${RUNNER_OSTYPE}.zip"
+# curl -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/terraform_${TERRAFORM_VERSION}_${RUNNER_OSTYPE}.zip"
+
+# ---------------------------------------------
+# TFLINT from https://github.com/terraform-linters/tflint/tags
+
+TFLINT_VERSION=0.55.1
+
+# for Linux local machine
+# curl -L https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/tflint_${TFLINT_VERSION}_${OSTYPE}.zip"
+
+# for Windows local machine
+curl -L "https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_windows_amd64.zip" -o "${GIT_ROOT_DIR}/bin/tflint_${TFLINT_VERSION}_${OSTYPE}.zip"
+
+# for MacOS local machine
+# curl -L "https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_darwin_amd64.zip" -o "${GIT_ROOT_DIR}/bin/tflint_${TFLINT_VERSION}_${OSTYPE}.zip"
+
+# For Github runner pipeline
+curl -L "https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip" -o "${GIT_ROOT_DIR}/bin/tflint_${TFLINT_VERSION}_${RUNNER_OSTYPE}.zip"
